85bf0cf30ef1eddea6308d868f204ad2c71b030b
marriet-aho <marrietaho2020@gmail.com>
2024-11-25 15:39:37 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,5 @@
+let name = prompt("what is your name?");
+let age = prompt("how old are you?");
+let email = prompt("what is your email");
+
+alert(` Hi ${name}, you are ${age} years old and we will keep in touch via your email: ${email}. 👋👋🫶✍️` )
